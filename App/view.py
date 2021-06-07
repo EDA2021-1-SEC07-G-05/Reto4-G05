6df88fee588f6ca9b959d71cbc765ad67a502fdd
Iván Camilo Ballén Méndez <68608786+iballen@users.noreply.github.com>
2021-06-06 20:23:25 -0500

    Entrega Final

--- App/view.py
+++ App/view.py
@@ -33,6 +33,8 @@ La vista se encarga de la interacción con el usuario
 Presenta el menu de opciones y por cada seleccion
 se hace la solicitud al controlador para ejecutar la
 operación solicitada
+
+ENTREGA FINAL
 """
 
 def execute_consulta_carga(catalog):
